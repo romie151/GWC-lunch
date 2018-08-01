0de6594bc505b48d701d7a410c017ff90b79c07b
romie151 <romie151@yahoo.com>
2018-08-01 15:54:17 -0400

    first commit

---
+++ lunch.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var sand1 = document.getElementById('mondayImage');
+    sand1.addEventListener('click', function (e) {
+        // document.getElementById('mondayImage').src = "./images/sand.jpg"
+
+        if (document.getElementById('mondayImage').src === "./images/sandwich.jpg") {
+           return  document.getElementById('mondayImage').src = "./images/sand.jpg"
+        } else if (document.getElementById('mondayImage').src === "./images/sand.jpg") {
+            return document.getElementById('mondayImage').src === "./images/sandwich.jpg"
+        }
+    })
+} )
