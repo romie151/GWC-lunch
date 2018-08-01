2187052b760207165c995eb2a16310220251b278
romie151 <romie151@yahoo.com>
2018-08-01 15:57:14 -0400

    update

--- lunch.js
+++ lunch.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var sand1 = document.getElementById('mondayImage');
     sand1.addEventListener('click', function (e) {
-        // document.getElementById('mondayImage').src = "./images/sand.jpg"
+        document.getElementById('mondayImage').src = "./images/sand.jpg"
 
-        if (document.getElementById('mondayImage').src === "./images/sandwich.jpg") {
-           return  document.getElementById('mondayImage').src = "./images/sand.jpg"
-        } else if (document.getElementById('mondayImage').src === "./images/sand.jpg") {
-            return document.getElementById('mondayImage').src === "./images/sandwich.jpg"
-        }
+        // if (document.getElementById('mondayImage').src === "./images/sandwich.jpg") {
+        //    return  document.getElementById('mondayImage').src = "./images/sand.jpg"
+        // } else if (document.getElementById('mondayImage').src === "./images/sand.jpg") {
+        //     return document.getElementById('mondayImage').src === "./images/sandwich.jpg"
+        // }
     })
 } )
