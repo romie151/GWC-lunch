236d99708c8effe8af7144618255b85e32beef2e
romie151 <romie151@yahoo.com>
2018-08-06 13:46:55 -0400

    fixing the toggle

--- lunch.js
+++ lunch.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
     sand1.addEventListener('click', function (e) {
         // sand1.src = "./images/sand.jpg"
         console.log(sand1.src)
-        if (sand1.src === "file:///C:/Users/GWC10/Documents/GWC-lunch/images/sandwich.jpg") {
-            sand1.src = "file:///C:/Users/GWC10/Documents/GWC-lunch/images/sand.jpg";
-        } else if (sand1.src === "file:///C:/Users/GWC10/Documents/GWC-lunch/images/sand.jpg") {
-            sand1.src = "file:///C:/Users/GWC10/Documents/GWC-lunch/images/sandwich.jpg";
+        if (sand1.src === "sandwich.jpg") {
+            sand1.src = "sand.jpg";
+        } else if (sand1.src === "sand.jpg") {
+            sand1.src = "sandwich.jpg";
         }
     });
 });
