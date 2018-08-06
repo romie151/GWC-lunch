288338ae922447b7bf37f6e66c4ea4e13a58f58c
romie151 <romie151@yahoo.com>
2018-08-06 15:35:27 -0400

    I'M UPLOADING SOME OTHER SHIT I DID TODAY

--- lunch.js
+++ lunch.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var sand1 = document.getElementById('mondayImage');
-   
+
     sand1.addEventListener('click', function (e) {
         // sand1.src = "./images/sand.jpg"
         console.log(sand1.src)
@@ -10,5 +10,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
             sand1.src = "sandwich.jpg";
         }
     });
+
+    fetch('http://api.example.com')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (obj) {
+        });
 });
 
